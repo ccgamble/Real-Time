4f167eeafb883c945e1692042aa608dfb56f428c
ccgamble <christine.c.gamble@gmail.com>
2017-02-06 00:08:35 -0700

    clicking on individual poll buttons renders innerText in console

--- public/poll.js
+++ public/poll.js
@@ -1,4 +1,8 @@
 const socket = io()
+const connectionCount = $('#connection-count');
+const statusMessage = $('#status-message');
+const buttons = document.querySelectorAll('.option-button')
+
 
 $(document).ready(function() {
 
@@ -16,28 +20,33 @@ $(document).ready(function() {
 
 function renderPoll(poll) {
 
-	console.log(poll)
-	if(!$('.btn-login').is(':visible')) {
-		$('.poll-container').hide()
-	} else {
-	  $('.poll-question').append(
-	    `<h3>${poll.data.question}</h3>`
-		);
-	  $('.poll-options').append(
-	    `<button class=option-button type=button>${poll.data.option1}</button>`
-	  );
-		$('.poll-options').append(
-			`<button class=option-button type=button>${poll.data.option2}</button>`
-		);
-		$('.poll-options').append(
-			`<button class=option-button type=button>${poll.data.option3}</button>`
-		);
-		$('.poll-options').append(
-			`<button class=option-button type=button>${poll.data.option4}</button>`
-		);
-	}
+	// if(!$('.btn-login').is(':visible')) {
+	// 	$('.poll-container').hide()
+	// } else {
+		$('.poll-question').text(poll.data.question)
+
+	  $('.button1').text(poll.data.option1)
+
+		$('.button2').text(poll.data.option2)
+
+		$('.button3').text(poll.data.option3)
+
+		$('.button4').text(poll.data.option4)
+
 }
 
 socket.on('usersConnected', (count) => {
-	$('#connection-count').append('Connected Users: ' + count);
+	connectionCount.text('Connected Users: ' + count);
 })
+
+socket.on('statusMessage', (message) => {
+	statusMessage.text(message)
+});
+
+
+
+	for (let i = 0; i < buttons.length; i++) {
+	  buttons[i].addEventListener('click', function() {
+	    console.log(this.innerText);
+	  });
+	}
