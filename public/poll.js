010b62093e54c370c5477a8b4c24095c1e66982d
ccgamble <christine.c.gamble@gmail.com>
2017-02-07 11:49:30 -0700

    displays photo by selected vote

--- public/poll.js
+++ public/poll.js
@@ -2,6 +2,7 @@ const socket = io()
 const connectionCount = $('#connection-count');
 const statusMessage = $('#status-message');
 const buttons = document.querySelectorAll('.option-button')
+const voteUpdate = $('#vote-update')
 
 
 $(document).ready(function() {
@@ -24,15 +25,22 @@ function renderPoll(poll) {
 	// } else {
 		$('.poll-question').text(poll.data.question)
 
-	  $('.button1').text(poll.data.option1)
+	  $('#button1').text(poll.data.option1)
 
-		$('.button2').text(poll.data.option2)
+		$('#button2').text(poll.data.option2)
 
-		$('.button3').text(poll.data.option3)
+		$('#button3').text(poll.data.option3)
 
-		$('.button4').text(poll.data.option4)
-
+		$('#button4').text(poll.data.option4)
 }
+
+$(document).on('click', '.option-button', function() {
+	let id = $(this).attr('id')
+	let photo = localStorage.getItem('photo')
+	let name = localStorage.getItem('name')
+
+	socket.emit('voteCast', id, photo, name)
+})
 
 socket.on('usersConnected', (count) => {
 	connectionCount.text('Connected Users: ' + count);
@@ -41,28 +49,42 @@ socket.on('usersConnected', (count) => {
 socket.on('statusMessage', (message) => {
 	statusMessage.text(message)
 });
+//
+// socket.on('voteCount', (votes) => {
+//   console.log(votes);
+// });
 
 
+socket.on('voteUpdate', (id, image, votes) => {
+	console.log(votes)
 
-	for (let i = 0; i < buttons.length; i++) {
-	  buttons[i].addEventListener('click', function() {
-	    socket.send('voteCast', this.innerText)
-	  });
-	}
+	// $('.vote-img').remove()
+	votes.map(vote => {
+		$('.vote-photo').remove()
+		return $(`.${vote.button_id}_results`).append(`<img
+														src=${vote.photo}
+														alt="user image"
+														class="vote-photo"
+													/>`)
+	})
 
-	socket.on('voteCount', (votes) => {
-	countVotes(votes)
-  // console.log(votes);
+
 })
 
-	function countVotes(votes) {
+//
+// 	socket.on('voteCount', (votes) => {
+// 	countVotes(votes)
+//   // console.log(votes);
+// })
 
-		voteCount1 = 0
-		return Object.values(votes).filter(function(option) {
-			// debugger
-			if (option === $('.button1').text()) {
-				voteCount1 =+ 1
-		}
-		console.log(voteCount1)
-		})
-	}
+	// function countVotes(votes) {
+	//
+	// 	voteCount1 = 0
+	// 	return Object.values(votes).filter(function(option) {
+	// 		// debugger
+	// 		if (option === $('.button1').text()) {
+	// 			voteCount1 =+ 1
+	// 	}
+	// 	console.log(voteCount1)
+	// 	})
+	// }
