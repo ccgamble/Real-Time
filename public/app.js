167891873acb212802104ade6bc4f57c61a4d55c
ccgamble <christine.c.gamble@gmail.com>
2017-02-11 17:21:44 -0700

    swithed to spacees for js files and added soft tabs

--- public/app.js
+++ public/app.js
@@ -1,27 +1,27 @@
 $('.poll-submit-btn').on('click', function(e) {
-	e.preventDefault();
-	
-	const question = $('.poll-question-input').val();
-	const option1 = $('.option-input1').val();
-	const option2 = $('.option-input2').val();
-	const option3 = $('.option-input3').val();
-	const option4 = $('.option-input4').val();
+  e.preventDefault();
+  
+  const question = $('.poll-question-input').val();
+  const option1 = $('.option-input1').val();
+  const option2 = $('.option-input2').val();
+  const option3 = $('.option-input3').val();
+  const option4 = $('.option-input4').val();
 
-	$.ajax({
-		url:'/api/poll',
-		type: 'post',
-		dataType: 'json',
-		data: {
-			question: question,
-			option1: option1,
-			option2: option2,
-			option3: option3,
-			option4: option4
-		},
-		success: postURL
-	});
+  $.ajax({
+    url:'/api/poll',
+    type: 'post',
+    dataType: 'json',
+    data: {
+      question: question,
+      option1: option1,
+      option2: option2,
+      option3: option3,
+      option4: option4
+    },
+    success: postURL
+  });
 });
 
 const postURL = (response) => {
-	$('body').append(`<a href ='/poll/?poll=${response.poll.id}'>Poll Link</a>`);
+  $('body').append(`<a href ='/poll/?poll=${response.poll.id}'>Poll Link</a>`);
 };
